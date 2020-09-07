bed9dcbad00918b292d4aded5e2ead4581172c9a
kharchenkok <k.v.kharchenko@ukr.net>
2020-09-07 17:27:06 +0300

    Updates

---
+++ precache-manifest.44b3c7e4539414976ae0f0a4d05a86f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337ee8ee8f7804fcb6eb0aff626982a9",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "f94736f616923c094852",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.1f8a8c63.chunk.css"
+  },
+  {
+    "revision": "abde9ee02ad3a44ab688",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.99c6b650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.99c6b650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94736f616923c094852",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.e0da6903.chunk.js"
+  },
+  {
+    "revision": "e721b7c80b982967a10e",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.f23c51c3.js"
+  }
+]);
